22b63250b88eeead2f866b9ac034a8dd123b5eb3
vientranquangminh <81664187+vientranquangminh@users.noreply.github.com>
2022-09-21 00:31:09 +0700

    Revert "my appointment screen"

--- lib/src/pages/screens/history/history_page.dart
+++ lib/src/pages/screens/history/history_page.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:my_health_assistant/src/widgets/custom_appbar/custom_appbar.dart';
 
 class ReportPage extends StatelessWidget {
   const ReportPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(title: 'History', 
-      actions: const ['assets/images/schedule_page/search.svg',
-      'assets/images/schedule_page/more.svg',],),
-      body: const Center(
-        child: Text('This is History Page'),
-      ),
+    return const Center(
+      child: Text('This is History Page'),
     );
   }
 }
