afbaae4a220a12a2c3c597efb2a56e17152af3f1
vientranquangminh <vienquangminh14@gmail.com>
2022-09-20 23:44:19 +0700

    udpate app bar action

--- lib/src/widgets/custom_appbar/app_bar_action.dart
+++ lib/src/widgets/custom_appbar/app_bar_action.dart
@@ -7,24 +7,16 @@ import 'package:flutter_svg/svg.dart';
 class AppBarAction extends StatelessWidget {
   static const actionHomePage = 'actionHomePage';
 
-
   final String action;
 
   const AppBarAction(this.action, {super.key});
+
   @override
   Widget build(BuildContext context) {
-    switch (action) {
-      case actionHomePage:
-        return IconButton(
-            onPressed: () {
-              log("HomePage");
-            },
-            icon: SvgPicture.asset(''));
-      default:
-        return IconButton(
-          onPressed: () {},
-          icon: SvgPicture.asset(""),
-        );
-    }
+    return IconButton(
+        onPressed: () {
+          log("HomePage");
+        },
+        icon: SvgPicture.asset(action));
   }
 }
